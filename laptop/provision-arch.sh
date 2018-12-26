40a589a31b2630233c4675cdcebd1b3483612397
Markus Schwarz <post@markus-schwarz.net>
2018-12-26 23:08:44 +0100

    add steam dependencies

--- laptop/provision-arch.sh
+++ laptop/provision-arch.sh
@@ -7,8 +7,9 @@ sudo pacman -Sy --noconfirm --needed $SYSTEM
 TOOLS="duplicity vim terminator powerline powerline-fonts"
 APPS="thunderbird inkscape gimp filezilla"
 DEVELOP="docker docker-compose jdk-openjdk code maven"
+STEAM="xf86-video-intel lib32-mesa ttf-liberation"
 
-sudo pacman -Sy --noconfirm --needed $TOOLS $APPS $DEVELOP
+sudo pacman -Sy --noconfirm --needed $TOOLS $APPS $DEVELOP $STEAM
 
 BUILD_MANUALLY_REPOS="pikaur"
 AUR_DIR="$HOME/git/aur"
